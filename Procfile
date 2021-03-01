2ab4c7f353ddce92d6a6db299c453d1ed130bfa7
MGniad <Marek.j.gniady@gmail.com>
2021-03-01 12:49:08 +0100

    Aditional commit

---
+++ Procfile
@@ -0,0 +1,5 @@
+relese: python manage.py makemigrations --no-input
+relese: python manage.py migrate --no-input
+
+web: gunicorn forum.wsgi
+
--- forum/settings.py
+++ forum/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9yaqn@9cgrcck_wu20!9j5-pb!$f&ippvhd##q*_^8g=1)8=qe'
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
